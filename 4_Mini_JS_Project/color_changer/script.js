f0ba95d327556a7d2f3ca8912507a90f966de387
riteshjha <rj34567@gmail.com>
2024-01-10 23:09:54 +0530

    Done color changer task

--- 4_Mini_JS_Project/color_changer/script.js
+++ 4_Mini_JS_Project/color_changer/script.js
@@ -1,2 +1,23 @@
 const btn = document.querySelectorAll('.Button');
-document.querySelector('body')
+const body = document.querySelector('body');
+
+btn.forEach(function(Button){
+    console.log(Button);
+    Button.addEventListener("click", function(e){
+        console.log(e)
+        console.log(e.target)
+        if(e.target === yellow ){
+          body.style.backgroundColor = e.target.id
+        }
+        if(e.target === blue ){
+            body.style.backgroundColor = e.target.id
+          }
+          if(e.target === white ){
+            body.style.backgroundColor = e.target.id
+          }
+          if(e.target === gray ){
+            body.style.backgroundColor = e.target.id
+          }
+    });
+});
+
